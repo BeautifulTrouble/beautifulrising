5e99fd3311007136c48b50c44fb087f218572fe9
Adrian Carpenter <adriatic.c@gmail.com>
2014-08-10 22:24:50 -0700

    Add functional but incomplete mobile menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('#menu-button').click(function (e) {
+    $('#menu-overlay').toggleClass('hide');
+    e.preventDefault();
+});
+$('#menu-close').click(function (e) {
+    $('#menu-overlay').toggleClass('hide');
+    e.preventDefault();
+});
