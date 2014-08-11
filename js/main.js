5ba39b78843b4bc8f068e1f0417c714678f3ab3f
Adrian Carpenter <adriatic.c@gmail.com>
2014-08-11 14:33:23 -0700

    Various responsive nav kludges and fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $('#menu-button').click(function (e) {
+    if ($(document.body).hasClass('page-beautiful-rising')) {
+        $('#menu-update').addClass('active');
+        $('#menu-update-content').removeClass('hide');
+    }
     $('#menu-overlay').toggleClass('hide');
     e.preventDefault();
 });
@@ -6,3 +10,9 @@ $('#menu-close').click(function (e) {
     $('#menu-overlay').toggleClass('hide');
     e.preventDefault();
 });
+$('#menu-update').click(function (e) {
+    $(this).toggleClass('active');
+    $('#menu-update-content').toggleClass('hide');
+    e.preventDefault();
+});
+
